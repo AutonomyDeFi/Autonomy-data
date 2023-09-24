c02dcf3f328443a54c77412501e648c58b54c242
Your Name <you@example.com>
2023-09-23 23:49:39 -0400

    moidell

--- autonomy/agent/agent.py
+++ autonomy/agent/agent.py
@@ -8,10 +8,12 @@ class Agent(a.Block):
     """
 
     def __init__(self,
+                model='gpt-3.5-turbo', 
                 tools=None,
                 memory = {} #TODO
                 ):
         self.set_tools(tools)
+        self.model = model
         self.memory = memory
         self.model = a.block('llm.openai')()
 
@@ -35,7 +37,6 @@ class Agent(a.Block):
         Suggest ONE tool to use for this task. Return in the format:
 
         ANSWER:
-
         {{tool_name:str, tool_kwargs:dict)}}
 
         '''json
@@ -45,14 +46,20 @@ class Agent(a.Block):
         prompt = json.dumps(prompt)
         return prompt
 
-    def call(self, task: str, max_steps=10, max_tokens=256, model='gpt-3.5-turbo') -> str:
+    def call(self, task: str,
+              max_tokens=256, 
+              max_trials:int=3) -> str:
 
+        try:
+            prompt = self.prompt(task=task)
+            r = self.model.chat(prompt, model=self.model, max_tokens=max_tokens)
+            r = json.loads(r.split('ANSWER:')[1].strip())
+            result = self.tools[r['tool_name']].call(**r['tool_kwargs'])
+        except Exception as e:
+            result = {'success': False, 'msg': f"Error: {e}"}
+            if 
+            a.print(result)
 
-        prompt = self.prompt(task=task)
-        r = self.model.chat(prompt, model=model, max_tokens=max_tokens)
-        a.print(r)
-        r = json.loads(r.split('ANSWER:')[1].strip())
-        result = self.tools[r['tool_name']].call(**r['tool_kwargs'])
         return result
 
     @classmethod
